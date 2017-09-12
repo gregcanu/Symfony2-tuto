61f94108e777e85d5c395c72d3381c33e5519e53
GregC <canugregoire@gmail.com>
2017-09-12 15:19:32 +0200

    Routeur

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -10,21 +10,23 @@ use Symfony\Component\HttpFoundation\Response;
 class AdvertController extends Controller {
 
     public function indexAction() {
-        $content = $this
-                ->get('templating')
-                ->render('OCPlatformBundle:Advert:index.html.twig', array(
-                    'nom' => 'Greg'
-                )
-            );
-        return new Response($content);
+//        $content = $this
+//                ->get('templating')
+//                ->render('OCPlatformBundle:Advert:index.html.twig', array(
+//                    'nom' => 'Greg'
+//                )
+//            );
+        
+        // On veut avoir l'URL de l'annonce d'id 5.
+        $url = $this->get('router')->generate(
+            'oc_platform_view', // 1er argument : le nom de la route
+            array('id' => 5)    // 2e argument : les valeurs des paramètres
+        );
+        // $url vaut « /platform/advert/5 »
+        return new Response($url);
     }
     
-    public function byebyeAction() {
-        $content = $this->get('templating')
-                ->render('OCPlatformBundle:Advert:byebye.html.twig', array(
-                    'nom' => 'Gregzer'
-                ));
-        return new Response($content);
+    public function viewAction($id) {
+        return new Response("Affichage de la variable id : ".$id);
     }
-
 }
